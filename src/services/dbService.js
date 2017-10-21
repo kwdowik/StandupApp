611069dd0e78744bafe5872c3b5ff604bd62f5fb
kwdowik <wdowikkacper@gmail.com>
2017-10-21 22:08:06 +0200

    changeDbSource

--- src/services/dbService.js
+++ src/services/dbService.js
@@ -3,7 +3,7 @@ var mongodb = require('mongodb').MongoClient;
 var dbService = function () {
 
     var getUsers = function () {
-        var url = 'mongodb://localhost/standupdb';
+        var url = 'mongodb://kacper:kacper@ds155192.mlab.com:55192/standupdb';
         return new Promise(
             function (resolve, reject) {
                 mongodb.connect(url, function (err, db) {
